ece861597927b51808067817642aed147024ee63
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-11-21 23:22:53 +0300

    pending assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+function multiply(num1, num2) {
+    num1 * num2 === 62
+}
 
-// Write your code here
+function random(num) {
+    num = Math.random() > 0
+}
+
+function mod(num3, num4) {
+    let result
+    result = num3 % num4 === 4;
+} 
+function max() {
+    return Math.max() === 20
+}
