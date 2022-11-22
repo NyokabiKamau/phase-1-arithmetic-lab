99a55268d75226e0b74acc77fa4bb3eb0e87f664
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-11-22 14:21:51 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-function multiply(num1, num2) {
-    num1 * num2 === 62
-}
+//equation whose multiplied results will be 62
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
+console.log(multiply)
 
-function random(num) {
-    num = Math.random() > 0
-}
+const random = Math.ceil(Math.random()*10)+1
+console.log(random)
 
-function mod(num3, num4) {
-    let result
-    result = num3 % num4 === 4;
-} 
-function max() {
-    return Math.max() === 20
-}
+let num3 = 20
+let num4 = 5
+const mod = num3 / num4
+console.log(mod)
+ 
+const max = 20
+    console.log(Math.max(max))
